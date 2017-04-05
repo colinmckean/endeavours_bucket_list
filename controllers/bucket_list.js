2a90a9abd45ce10ad8188bd8db5aa2a8d5a36429
Colin McKean <colinmckeandigital@gmail.com>
2017-04-05 16:07:52 +0100

    this.

--- controllers/bucket_list.js
+++ controllers/bucket_list.js
@@ -2,7 +2,11 @@ var express = require('express');
 var app = express();
 var bucketRouter = express.Router();
 
+var bodyParser = require('body-parser');
+bucketRouter.use(bodyParser.json());
+bucketRouter.use(bodyParser.urlencoded({extended: true}));
 
+var Country = require('../client/src/models/country');
 var BucketListQuery = require('../client/db/BucketListQuery');
 
 var query = new BucketListQuery();
@@ -13,11 +17,17 @@ bucketRouter.get('/', function(req, res){
   });
 });
 
-// bucketRouter.get('/', function(req, res)
-// {
-//   // res.sendFile(path.join(__dirname + 'index.html'));
-//   res.json({data: 'Hello!'});
-// }
-// );
+
+bucketRouter.post('/', function(req, res){
+
+  console.log(req.body);
+  var country = new Country({
+    name: req.body.name
+  });
+
+  query.add(country, function(results){
+    res.json(results);
+  })
+});
 
 module.exports = bucketRouter;
